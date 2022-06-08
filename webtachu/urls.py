ff2d9034ad39fc5d2a083d3d81b867a0315083f6
yinmsk <galaxy220111@gmail.com>
2022-06-08 09:27:38 +0900

    Revert "프로젝트 생성"
    This reverts commit e260212a0bbda8252fb76b7dadc779315c0e5b38.

--- webtachu/urls.py
+++ webtachu/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('', include('books.urls')),
+    path('', include('users.urls'))
 ]
