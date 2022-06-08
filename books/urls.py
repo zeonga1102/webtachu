c341ef607587b81c0c5792fc52426d9d6287a103
yinmsk <galaxy220111@gmail.com>
2022-06-08 21:20:52 +0900

    CLN

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,7 @@ from . import book_views
 
 urlpatterns = [
     path('genre/<str:name>', views.genre_view, name='genre'),
-    path('genre/<str:name>/<int:page>', views.genre_paginator, name='genre_paginator'),
+    path('genre/<str:name>/<int:page>', views.genre_view, name='genre_paginator'),
     path('<int:id>/', book_views.detail_view, name='book_info'),
     path('main', views.main_view, name='main'),
 ]
--- books/views.py
+++ books/views.py
@@ -3,12 +3,12 @@ from books.models import BookModel
 from django.core.paginator import Paginator
 
 
-def genre_view(request, name):
-    books = BookModel.objects.filter(genre=name)
-    return render(request, 'main_genre/genre.html', {'books': books})
+# def genre_view(request, name):
+#     books = BookModel.objects.filter(genre=name)
+#     return render(request, 'main_genre/genre.html', {'books': books})
 
 
-def genre_paginator(request, name, page):
+def genre_view(request, name, page):
     books_list = BookModel.objects.filter(genre=name)
     paginator = Paginator(books_list, 10)
     pages = paginator.get_page(page)
