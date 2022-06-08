42425ea1319f7e8726c84b8e1a40f129ddf4b6b4
yinmsk <galaxy220111@gmail.com>
2022-06-08 21:21:33 +0900

    CLN

--- books/urls.py
+++ books/urls.py
@@ -5,8 +5,7 @@ from . import book_views
 
 
 urlpatterns = [
-    path('genre/<str:name>', views.genre_view, name='genre'),
-    path('genre/<str:name>/<int:page>', views.genre_view, name='genre_paginator'),
+    path('genre/<str:name>', views.genre_view, name='books'),
+    path('home', views.genre_view, name='books'),
     path('<int:id>/', book_views.detail_view, name='book_info'),
-    path('main', views.main_view, name='main'),
 ]
