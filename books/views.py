e7d951a96689c58556001cb4a6569f58088d856e
yinmsk <galaxy220111@gmail.com>
2022-06-09 01:00:18 +0900

    ENH 페이지네이션 기능 추가

--- books/views.py
+++ books/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from books.models import BookModel
+from django.core.paginator import Paginator
 
+
+# def genre_view(request, name):
+#     books = BookModel.objects.filter(genre=name)
+#     return render(request, 'main_genre/genre.html', {'books': books})
 
 def genre_view(request, name):
-    books = BookModel.objects.filter(genre=name)
-    return render(request, 'main_genre/genre.html', {'books': books})
+    books_list = BookModel.objects.filter(genre=name)
+    page = request.GET.get('page', '1')
+    paginator = Paginator(books_list, 10)
+    pages = paginator.page(page)
+    return render(request, 'main_genre/genre.html', {'pages': pages})
+
+
+def main_view(request):
+    return render(request, 'main_genre/main.html')
