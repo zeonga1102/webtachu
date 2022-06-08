f341f01a918210905cc7fe32a46cb3f2a57e9570
yinmsk <galaxy220111@gmail.com>
2022-06-08 20:59:30 +0900

    ENH 페이지내이션 베타버전

--- books/views.py
+++ books/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from books.models import BookModel
+from django.core.paginator import Paginator
 
 
 def genre_view(request, name):
     books = BookModel.objects.filter(genre=name)
     return render(request, 'main_genre/genre.html', {'books': books})
+
+
+def genre_paginator(request, name, page):
+    books_list = BookModel.objects.filter(genre=name)
+    paginator = Paginator(books_list, 10)
+    pages = paginator.get_page(page)
+    return render(request, 'main_genre/genre.html', {'pages': pages})
+
+
+def main_view(request):
+    return render(request, 'main_genre/main.html')
