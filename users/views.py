7b6b7c42512131d2d3a2517ebc2341e315a7570b
zeonga1102 <dlwjddk4587@naver.com>
2022-06-09 19:06:41 +0900

    ENH: 마이페이지에서 선호작 누른 책들의 키워드 생성하는 기능 추가

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from .models import UserModel, ReviewModel, BookModel
 from django.contrib.auth import get_user_model
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from books.book_views import make_keyword
 
 
 # Create your views here.
@@ -64,12 +65,18 @@ def home(request):
 def mypage(request):
     if request.method == 'GET':
         user = request.user
-        # favorite = user.favorite.all().order_by('-id')[:5]
-        favorite = user.favorite.all()[::-1][:5]
-        reviews = ReviewModel.objects.filter(user_id=user.id)[::-1][:3]
+        review_data = ReviewModel.objects.filter(user=user)
+        favorite_data = user.favorite.all()
 
-        fav_cnt = user.favorite.all().count()
-        review_cnt = ReviewModel.objects.filter(user_id=user.id).count()
+        favorite = favorite_data[::-1][:5]
+        reviews = review_data[::-1][:3]
+
+        fav_cnt = favorite_data.count()
+        review_cnt = review_data.count()
         count = {'fav':fav_cnt, 'rev':review_cnt}
 
+        keyword = make_keyword(favorite_data, 'story', 10)
+        print(keyword)
+
         return render(request, 'user/mypage.html', {'reviews': reviews, 'favorite': favorite, 'count':count})
+
