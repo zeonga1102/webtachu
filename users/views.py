58ec286b57d837ef6459588c079e0b939ad6b643
m_ink <mp98dl4@gmail.com>
2022-06-09 21:10:20 +0900

    ENH: mypage 선호작품 등록기준 최신순 정렬 기능 구현

--- users/views.py
+++ users/views.py
@@ -70,28 +70,29 @@ def mypage(request):
 
         user_id = user.id
         cursor = connection.cursor()
-        # query = "SELECT * FROM users_favorite"
         query = "SELECT * FROM users_favorite WHERE usermodel_id=%s" % (user_id)
         result = cursor.execute(query)
         stocks = cursor.fetchall()
-        context = {'stocks': stocks}
-        print(context)
+
+        origin_stocks = stocks
+        stocks.sort(key=lambda x: -x[0])
 
         review_data = ReviewModel.objects.filter(user=user)
         favorite_data = user.favorite.all()
 
-        favorite = favorite_data[::-1][:5]
+        favorite = []
+        for i in range(5):
+            favorite.append(user.favorite.get(id=stocks[i][2]))
         reviews = review_data[::-1][:3]
 
-        fav_cnt = favorite_data.count()
+        fav_cnt = len(origin_stocks)
         review_cnt = review_data.count()
         count = {'fav':fav_cnt, 'rev':review_cnt}
 
         keyword = make_keyword(favorite_data, 'story', 10)
         print(keyword)
 
-        for review in review_data:
+        for review in reviews:
             review.star = review.star * 20
 
-    return render(request, 'user/mypage.html', {'reviews': reviews, 'favorite': favorite, 'count':count})
-
+    return render(request, 'user/mypage.html', {'reviews': reviews, 'favorite': favorite, 'count':count, 'keyword':keyword})
