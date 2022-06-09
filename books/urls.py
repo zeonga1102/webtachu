1cd2932d7b68651aaa5130c405c355d8ff99d2f1
zeonga1102 <dlwjddk4587@naver.com>
2022-06-09 09:16:34 +0900

    BUG: main 페이지 함수 연결 잘못된 것 수정

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,7 @@ from . import book_views
 urlpatterns = [
     path('genre/<str:name>', views.genre_view, name='genre'),
     path('genre/<str:name><int:page>', views.genre_view, name='paginator'),
-    path('main', views.genre_view, name='main'),
+    path('main', views.main_view, name='main'),
 
     path('<int:id>/', book_views.detail_view, name='book_info'),
     path('favorite/<int:id>', book_views.book_favorite, name='book_favorite'),
