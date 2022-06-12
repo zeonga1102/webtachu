407d078ff44ef65ae36462fabd79c22e427d5770
zeonga1102 <dlwjddk4587@naver.com>
2022-06-12 19:42:24 +0900

    CLN

--- books/views.py
+++ books/views.py
@@ -89,8 +89,7 @@ def create_review(request, book_id):
         ReviewModel.objects.create(user=user, book=current_book, star=star, desc=review)
 
         new_avg = (current_book.star * review_count + star) / (review_count + 1)
-        new_avg = round(new_avg, 1)
-        current_book.star = new_avg
+        current_book.star = round(new_avg, 1)
         current_book.save()
 
         return redirect('book_info', book_id)
@@ -130,8 +129,7 @@ def modify_review(request, book_id, review_id):
         current_book = BookModel.objects.get(id=book_id)
         review_count = ReviewModel.objects.filter(book=current_book).count()
         new_avg = (current_book.star * review_count - origin_review[0].star + star) / review_count
-        new_avg = round(new_avg, 1)
-        current_book.star = new_avg
+        current_book.star = round(new_avg, 1)
         current_book.save()
 
         origin_review.update(star=star, desc=review, date=date)
